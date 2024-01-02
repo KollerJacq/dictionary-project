67eda2234aa3cde06df9d8d28fc1044ce16f27f9
KollerJacq <147981831+KollerJacq@users.noreply.github.com>
2024-01-02 14:08:46 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import city from "./city.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <img src={city} className="img-city img-fluid" alt="img-city" />
+    </div>
+  );
+}
+
+export default App;
